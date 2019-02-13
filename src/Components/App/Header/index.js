ec839c4ffa3e02aa67d6cd707adf147be4e09780
amiloszewicz <miloszewicz.adam@gmail.com>
2019-02-13 19:05:09 +0100

    feat App & Header components

---
+++ src/Components/App/Header/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css'
+
+class Header extends Component {
+  render() {
+    return(
+      <header>
+        <h1>isBike</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
