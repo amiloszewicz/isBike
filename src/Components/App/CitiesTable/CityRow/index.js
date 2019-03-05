19ca28cb60f17eeeecfd248e5dda0c220ecda832
amiloszewicz <miloszewicz.adam@gmail.com>
2019-03-05 15:01:10 +0100

    feat get city network data in log after click city name

--- src/Components/App/CitiesTable/CityRow/index.js
+++ src/Components/App/CitiesTable/CityRow/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 
 class CityRow extends React.Component {
+  abc = (e) => {
+    console.log(`clicked`);
+    console.log(this.props.network);
+
+    axios
+      .get(`https://api.citybik.es/v2/networks/${this.props.network.id}`)
+      .then(response =>
+        console.log(response.data.network.stations)
+      )
+      .catch(error => this.setState({ error }));
+  };
+
   render() {
-    return <p>{this.props.name}</p>;
+    return (
+      <p onClick={this.abc}>{this.props.cityName}</p>
+    );
   }
 }
 
--- src/Components/App/CitiesTable/index.js
+++ src/Components/App/CitiesTable/index.js
@@ -8,7 +8,7 @@ class CitiesTable extends Component {
   render() {
     const networks = this.props.results.map(network => network);
     const queryNetworks = networks.filter(network =>
-      network.location.city.includes(this.props.query)
+      network.location.city.toLowerCase().includes(this.props.query, 0)
     );
 
     return (
@@ -17,7 +17,7 @@ class CitiesTable extends Component {
         <ul>
           {queryNetworks.map(network => (
             <li key={network.id}>
-              <CityRow name={network.location.city} />
+              <CityRow cityName={network.location.city} network={network} />
             </li>
           ))}
         </ul>
