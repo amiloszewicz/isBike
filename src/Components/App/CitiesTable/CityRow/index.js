241276ab0fbe802d2ea41208948e9c3a4dd76f65
amiloszewicz <miloszewicz.adam@gmail.com>
2019-03-12 14:56:46 +0100

    refactor city row component

--- src/Components/App/CitiesTable/CityRow/index.js
+++ src/Components/App/CitiesTable/CityRow/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import axios from 'axios';
+import StationsTable from './StationsTable';
 
 class CityRow extends React.Component {
-  abc = (e) => {
-    console.log(`clicked`);
-    console.log(this.props.network);
-
-    axios
-      .get(`https://api.citybik.es/v2/networks/${this.props.network.id}`)
-      .then(response =>
-        console.log(response.data.network.stations)
-      )
-      .catch(error => this.setState({ error }));
-  };
-
   render() {
     return (
-      <p onClick={this.abc}>{this.props.cityName}</p>
+      <>
+        <StationsTable
+          cityName={this.props.cityName}
+          network={this.props.network}
+        />
+      </>
     );
   }
 }
