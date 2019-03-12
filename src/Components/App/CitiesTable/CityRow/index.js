487c1e386b030945c4ad5bb17c896bc3076a6053
amiloszewicz <miloszewicz.adam@gmail.com>
2019-03-12 15:46:54 +0100

    feat fetch stations list for clicked city

--- src/Components/App/CitiesTable/CityRow/index.js
+++ src/Components/App/CitiesTable/CityRow/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import axios from 'axios';
+
 import StationsTable from './StationsTable';
 
 class CityRow extends React.Component {
+  state = {
+    stations: []
+  };
+
+  getStationsList = () => {
+    axios
+      .get(`https://api.citybik.es/v2/networks/${this.props.network.id}`)
+      .then(response =>
+        this.setState({
+          stations: response.data.network.stations
+        })
+      )
+      .catch(error => this.setState({ error }));
+  };
+
   render() {
     return (
-      <>
-        <StationsTable
-          cityName={this.props.cityName}
-          network={this.props.network}
-        />
-      </>
+      <div onClick={this.getStationsList}>
+        {this.props.cityName}
+        { (this.state.stations.length > 0) ? (<StationsTable stations={this.state.stations} />) : null }
+      </div>
     );
   }
 }
