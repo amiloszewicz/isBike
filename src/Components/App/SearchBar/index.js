aded4b603c6ee78aeb216584c4f730f2ff6452bb
amiloszewicz <miloszewicz.adam@gmail.com>
2019-03-12 14:56:13 +0100

    refactor search bar component

--- src/Components/App/SearchBar/index.js
+++ src/Components/App/SearchBar/index.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import CitiesTable from '../CitiesTable';
 
-import axios from 'axios';
+import axios from "axios";
 
-import './style.css';
+import "./style.css";
 
 class SearchBar extends Component {
   state = {
-    query: '',
+    query: "",
     results: []
   };
 
   getInfo = () => {
     axios
-      .get('https://api.citybik.es/v2/networks')
+      .get("https://api.citybik.es/v2/networks")
       .then(response =>
         this.setState({
           results: response.data.networks
@@ -23,28 +23,25 @@ class SearchBar extends Component {
   };
 
   handleInputChange = () => {
-    this.setState(
-      {
-        query: this.search.value
-      },
-      () => {
-        if (this.state.query && this.state.query.length > 1) {
-          this.getInfo();
-        }
+    this.setState({
+      query: this.search.value
+    }, () => {
+      if (this.state.query && this.state.query.length > 1) {
+        this.getInfo()
       }
-    );
+    });
   };
 
   render() {
     return (
       <form>
         <input
-          placeholder='Search for city...'
+          placeholder="Search for city..."
           ref={input => (this.search = input)}
           onChange={this.handleInputChange}
-          type='text'
+          type="text"
         />
-        <CitiesTable results={this.state.results} query={this.state.query} />
+        <CitiesTable results={this.state.results} query={this.state.query}/>
       </form>
     );
   }
