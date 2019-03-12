21d2f98e332825e57744b0fd545f5c4fd7e73ce1
amiloszewicz <miloszewicz.adam@gmail.com>
2019-03-12 15:49:07 +0100

    show all stations

--- src/Components/App/CitiesTable/CityRow/StationsTable/index.js
+++ src/Components/App/CitiesTable/CityRow/StationsTable/index.js
@@ -1,34 +1,16 @@
 import React from 'react';
-import axios from 'axios';
+import StationRow from './StationRow';
 
 class StationsTable extends React.Component {
-  state = {
-    stations: []
-  };
-
-  getStationsList = () => {
-    axios
-      .get(`https://api.citybik.es/v2/networks/${this.props.network.id}`)
-      .then(response =>
-        this.setState({
-          stations: response.data.network.stations
-        })
-      )
-      .catch(error => this.setState({ error }));
-  };
-
   render() {
-    const stations = this.state.stations;
-
     return (
-      <>
-        <p onClick={this.getStationsList}>{this.props.cityName}</p>
-        <ul>
-          {stations.map(station => (
-            <li key={station.id}>{station.name}</li>
-          ))}
-        </ul>
-      </>
+      <ul>
+        {this.props.stations.map(station => (
+          <li key={station.id}>
+            <StationRow stationName={station.name} />
+          </li>
+        ))}
+      </ul>
     );
   }
 }
