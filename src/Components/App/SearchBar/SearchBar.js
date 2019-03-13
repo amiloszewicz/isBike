4780ec624fac3b6e2f37204ba0bbf22d69210352
amiloszewicz <miloszewicz.adam@gmail.com>
2019-03-13 13:15:06 +0100

    change CitiesTable component with functions to different location

--- src/Components/App/SearchBar/SearchBar.js
+++ src/Components/App/SearchBar/SearchBar.js
@@ -1,44 +1,18 @@
-import React, { Component } from "react";
-import CitiesTable from '../CitiesTable/CitiesTable.js';
-import axios from "axios";
+import React, { Component, createRef } from "react";
 
 class SearchBar extends Component {
-  state = {
-    query: "",
-    results: []
-  };
-
-  getInfo = () => {
-    axios
-      .get("https://api.citybik.es/v2/networks")
-      .then(response =>
-        this.setState({
-          results: response.data.networks
-        })
-      )
-      .catch(error => this.setState({ error }));
-  };
-
-  handleInputChange = () => {
-    this.setState({
-      query: this.search.value
-    }, () => {
-      if (this.state.query && this.state.query.length > 1) {
-        this.getInfo()
-      }
-    });
-  };
+  inputElement = createRef();
 
   render() {
     return (
       <form>
         <input
           placeholder="Search for city..."
-          ref={input => (this.search = input)}
-          onChange={this.handleInputChange}
+          ref={this.inputElement}
+          onChange={this.props.onChange}
           type="text"
+          value={this.props.query}
         />
-        <CitiesTable results={this.state.results} query={this.state.query}/>
       </form>
     );
   }
