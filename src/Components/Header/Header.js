cc6a23b0142422ba4bc8c544441c2f81fb0c23f0
amiloszewicz <miloszewicz.adam@gmail.com>
2019-05-13 19:22:39 +0200

    style Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+import './Header.css';
+
 class Header extends Component {
   render() {
     return(
-      <header>
+      <header className="Header">
         <h1>isBike</h1>
       </header>
     );
