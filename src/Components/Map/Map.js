ecefd4ec7377921664b68516efc20dbe06a5cf71
admilo <miloszewicz.adam@gmail.com>
2019-06-17 22:17:02 +0200

    restyle zoomControl for map

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Map as LeafletMap, TileLayer, Marker, Popup, ZoomControl  } from 'react-leaflet';
 
 import './Map.css';
 
@@ -7,25 +7,26 @@ class Map extends Component {
   render() {
     return (
       <LeafletMap
-        center={this.props.position}
+        center={this.props.userLocation}
         zoom={this.props.zoom}
         maxZoom={10}
         attributionControl={true}
-        zoomControl={true}
+        zoomControl={false}
         doubleClickZoom={true}
         scrollWheelZoom={true}
         dragging={true}
         animate={true}
         easeLinearity={0.35}
       >
+        <ZoomControl position="bottomright" />
         <TileLayer url='http://{s}.tile.osm.org/{z}/{x}/{y}.png' />
         {this.props.haveUserLocation ? (
-          <Marker position={this.props.position}>
+          <Marker position={this.props.userLocation}>
             <Popup>Popup for any custom information.</Popup>
           </Marker>
         ) : (
-          ''
-        )}
+            ''
+          )}
       </LeafletMap>
     );
   }
