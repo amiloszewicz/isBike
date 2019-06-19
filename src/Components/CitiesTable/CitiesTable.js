d53e7d46e526c9035dc57e8c28ac50e588ba8d4b
amiloszewicz <miloszewicz.adam@gmail.com>
2019-06-19 14:56:10 +0200

    add comments to variables

--- src/Components/CitiesTable/CitiesTable.js
+++ src/Components/CitiesTable/CitiesTable.js
@@ -5,7 +5,18 @@ import './CitiesTable.css';
 
 class CitiesTable extends Component {
   render() {
+    /**
+     * creates new array of networks
+     * @param {array} results
+     */
     const networks = this.props.results.map(network => network);
+
+    /**
+     * Filter @param {array} networks
+     * to select @param {object} network
+     * with @param {string} query
+     * included in city name
+     */
     const queryNetworks = networks.filter(network =>
       network.location.city.toLowerCase().includes(this.props.query, 0)
     );
